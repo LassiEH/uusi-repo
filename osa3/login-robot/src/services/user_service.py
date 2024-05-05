6ca92f56b8a9638032728e540d31cc5ef4f1d498
LassiEH <lassi.haarala@live.fi>
2024-05-05 16:44:08 +0300

    osa4 demojen kohdat 1 ja 2

--- osa3/login-robot/src/services/user_service.py
+++ osa3/login-robot/src/services/user_service.py
@@ -1,4 +1,5 @@
 from entities.user import User
+import sys,pdb
 import re
 
 class UserInputError(Exception):
@@ -26,6 +27,9 @@ class UserService:
 
     def create_user(self, username, password):
         self.validate(username, password)
+        self.invalid_username(username)
+        self.short_password(password)
+        self.short_username(username)
 
         user = self._user_repository.create(
             User(username, password)
@@ -35,13 +39,25 @@ class UserService:
 
     def validate(self, username, password):
         if not username or not password:
-            raise UserInputError("Username and password are required")
+            raise Exception("Username and password are required")
 
         # toteuta loput tarkastukset tänne ja nosta virhe virhetilanteissa
 
+    def invalid_username(self, username):
         if not re.match("^[a-z]+$", username):
             print("Invalid characters in username")
-        
+            raise Exception("Invalid characters in username")
+    
+    def short_password(self, password):
         if len(password) < 8:
-            print("Password is too short")
+            raise Exception("Password is too short")
+
+    def short_username(self, username):
+        if len(username) < 3:
+            print("Username is too short")
+            raise Exception("Username is too short")
+        
+    def valid_password(self, password):
+        if len(password) <= 7 or re.match("^[a-z]+$", password):
+            raise Exception("Password not strong enough")
         
--- osa3/login-robot/src/tests/register.robot
+++ osa3/login-robot/src/tests/register.robot
@@ -4,13 +4,14 @@ Library    ../AppLibrary.py
 Resource    resource.robot
 Test Setup    Input New Command And Create user
 *** Test Cases ***
-Register With Valid Username And Password
+#Register With Valid Username And Password
 # ...
-    Input Credentials    kallek    kelakala10
-    Output Should Contain    New user registered
+#    Create User    kallek    kelakala10!
+#    Output Should Contain    New user registered
 
 Register With Already Taken Username And Valid Password
     Create User    velmuk    kolakala9
+    Create User    velmuk    jabadaba210
     Output Should Contain    User with username velmuk already exists
 
 Register With Too Short Username And Valid Password
@@ -38,3 +39,4 @@ Register With Valid Username And Long Enough Password Containing Only Letters
 Input New Command And Create user
     Input New Command
     Create User    velmuk    kokolala19
+    Input New Command
